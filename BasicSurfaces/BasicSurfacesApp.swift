a347ac0399236150764076f3b8c61fa1913a2a6c
Colin Ford <colin.ford@student.csulb.edu>
2024-08-16 19:36:37 -0700

    updating by deleting

---
+++ BasicSurfaces/BasicSurfacesApp.swift
@@ -0,0 +1,18 @@
+//
+//  BasicSurfacesApp.swift
+//  BasicSurfaces
+//
+//  Created by Colin Ford on 8/16/24.
+//  Copyright © 2024 Colin Ford. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct BasicSurfacesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BasicSurfaces/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  BasicSurfaces
+//
+//  Created by Colin Ford on 8/16/24.
+//  Copyright © 2024 Colin Ford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
